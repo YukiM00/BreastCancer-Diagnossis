c88e3d53c1bf69844c7e1b1e8535552f7b5373cc
YukiM00 <kitkitkitkit62@gmail.com>
2020-11-25 17:31:06 +0900

    only support vector machine

--- svm.r
+++ svm.r
@@ -1,28 +1,24 @@
-
+# Support Vector Machine
 wdbc <- read.csv("./data/wdbc.csv",header=T)
 
-#サポートベクターマシン
 library(kernlab)
 
-#データの準備
 d_s <- data.frame(scale(wdbc[c("radius","texture","perimeter")]),wdbc[c("diagnosis")])
 
-# ランダムに20個のデータのIDを得る。
+# Get the ID of 20 pieces of data at random.
 idx <- sample(1:length(d_s[[1]]),20)
-# モデル作成用のデータ（訓練データ）
+
 d_train <- d_s[idx,]
-# 検証データ
 d_test <- d_s[-idx,]
 
-#モデルの学習
+
 model <- ksvm(diagnosis ~ ., data=d_train, kernel="rbfdot")
 
 library(pROC)
-#データの予測
+
 pred <- predict(model, d_train, type="response")[,1]
 plot.roc(d_train$diagnosis, pred, print.auc = T)
 roc(d_train$diagnosis, pred)$auc
-
 
 pred <- predict(model, d_test, type="response")[,1]
 plot.roc(d_test[,c("diagnosis")], pred, print.auc = T)
