254a11aedb7a009d869aa87d28bc4465095599da
YukiM00 <kitkitkitkit62@gmail.com>
2020-11-25 17:26:00 +0900

    pred 3 methods

--- pred.r
+++ pred.r
@@ -1,55 +1,55 @@
-#データの準備
+#Preparation
 wdbc <- read.csv("./data/wdbc.csv")
 
 d_s <- data.frame(scale(wdbc[c("radius","texture","perimeter","area","smoothness","compactness","concavity","concave.points","symmetry","fractal.dimension")]),wdbc[c("diagnosis")])
 #"radius","texture","perimeter","area","smoothness","compactness","concavity","concave.points","symmetry","fractal.dimension"
 
-idx <- sample(1:length(d_s[[1]]),455)#      ランダムにデータを得る。
-d_train <- d_s[idx,]  #訓練用
-d_test <- d_s[-idx,]  #検証用
+idx <- sample(1:length(d_s[[1]]),455)#      
+d_train <- d_s[idx,]  #train
+d_test <- d_s[-idx,]  #test
 
-#ロジスティック回帰
-cat("ロジスティック回帰\n")
-model <- glm(diagnosis ~ ., family="binomial", data=d_train) #モデルの学習
+#Logistic Regression
+cat("Logistic Regression\n")
+model <- glm(diagnosis ~ ., family="binomial", data=d_train) 
 #model <- step(model1)
 summary(model)
 library(pROC)
-#データの予測(訓練)
+#prediction (train data)
 pred <- predict(model, d_train, type="response")
 plot.roc(d_train$diagnosis, pred, print.auc = T)
 roc(d_train$diagnosis, pred)$auc
-#データの予測(検証)
+#prediction (test data)
 pred <- predict(model, d_test, type="response")
 plot.roc(d_test$diagnosis, pred, print.auc = T)
 roc(d_test$diagnosis, pred)$auc
 
-#ランダムフォレスト
-cat("\n\n\n\nランダムフォレスト\n")
+#Random Forest
+cat("\n\n\n\nRandom Forest\n")
 library(randomForest)
-model1 <- randomForest(diagnosis ~ ., data=d_train, importance=T) #モデルの学習
+model1 <- randomForest(diagnosis ~ ., data=d_train, importance=T) 
 model <- step(model1)
 #importance(model)
 library(pROC)
-#データの予測(訓練)
+#prediction (train data)
 pred <- predict(model, d_train, type="response")
 plot.roc(d_train$diagnosis, pred, print.auc = T)
 roc(d_train$diagnosis, pred)$auc
-#データの予測(検証)
+#prediction (test data)
 pred <- predict(model, d_test, type="response")
 plot.roc(d_test[,c("diagnosis")], pred, print.auc = T)
 roc(d_test[,c("diagnosis")], pred)$auc
 
-#サポートベクターマシン
-cat("\n\n\n\nサポートベクターマシン\n")
+#Support Vector Machine
+cat("\n\n\n\nSupport Vector Machine\n")
 library(kernlab)
-model <- ksvm(diagnosis ~ ., data=d_train, kernel="rbfdot")#モデルの学習
+model <- ksvm(diagnosis ~ ., data=d_train, kernel="rbfdot")
 
 library(pROC)
-#データの予測(訓練)
+#prediction (train data)
 pred <- predict(model, d_train, type="response")[,1]
 plot.roc(d_train$diagnosis, pred, print.auc = T)
 roc(d_train$diagnosis, pred)$auc
-#データの予測(検証)
+#prediction (test data)
 pred <- predict(model, d_test, type="response")[,1]
 plot.roc(d_test[,c("diagnosis")], pred, print.auc = T)
 roc(d_test[,c("diagnosis")], pred)$auc
