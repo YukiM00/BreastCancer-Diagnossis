8b982eb5d427221eec651c657408a4ed80ba0899
YukiM00 <kitkitkitkit62@gmail.com>
2020-11-25 17:35:24 +0900

    Observe the relationship between the parameters

--- eval_data/pair.r
+++ eval_data/pair.r
@@ -1,3 +1,4 @@
+# Observe the relationship between the parameters
 wdbc <- read.csv("../data/wdbc.csv")
 d_s <- data.frame(scale(wdbc[c("radius","texture","perimeter","area","smoothness","compactness","concavity","concave.points","symmetry","fractal.dimension")]))
 library(psych)
